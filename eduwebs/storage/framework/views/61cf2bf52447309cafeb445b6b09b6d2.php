df80c6d8c4b118a869177845fcb6e86b92676cb3
Geryongko <152569528+Geryongko@users.noreply.github.com>
2025-11-10 00:12:27 +0700

    Add files via upload

---
+++ eduwebs/storage/framework/views/61cf2bf52447309cafeb445b6b09b6d2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mahasiswa</title>
+</head>
+<body>
+    <h1>Ini adalah page mahasiswa</h1>
+    <a href="<?php echo e(route('dsn')); ?>">Menu Dosen</a>
+</body>
+</html><?php /**PATH D:\Laravel 11\resources\views/mahasiswa.blade.php ENDPATH**/ ?>
---
+++ eduwebs/storage/framework/views/8c7c7ade6a0411ed7f0cc3ccf8fe424a.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dosen</title>
+</head>
+<body>
+    <h1>Ini adalah page dosen</h1>
+    <a href="<?php echo e(route('mhs')); ?>">menu mahasiswa</a>
+</body>
+</html><?php /**PATH D:\Laravel 11\resources\views/dosen.blade.php ENDPATH**/ ?>
